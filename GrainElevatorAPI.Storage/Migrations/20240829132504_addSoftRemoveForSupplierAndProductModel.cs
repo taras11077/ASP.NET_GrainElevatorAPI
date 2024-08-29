75d3dbbc95ccf9430feb0be77e1a6822a39fd2cc
stg11077 <stg11077@gmail.com>
2024-08-29 17:08:56 +0300

    add Migrations

---
+++ GrainElevatorAPI.Storage/Migrations/20240829132504_addSoftRemoveForSupplierAndProductModel.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrainElevator.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSoftRemoveForSupplierAndProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Suppliers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedById",
+                table: "Suppliers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Suppliers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedById",
+                table: "Suppliers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Removed",
+                table: "Suppliers",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RemovedAt",
+                table: "Suppliers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RemovedById",
+                table: "Suppliers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RestoreById",
+                table: "Suppliers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RestoredAt",
+                table: "Suppliers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedById",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedById",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Removed",
+                table: "Products",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RemovedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RemovedById",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RestoreById",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RestoredAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VehicleNumber",
+                table: "InputInvoices",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "InputInvoices",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suppliers_CreatedById",
+                table: "Suppliers",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suppliers_ModifiedById",
+                table: "Suppliers",
+                column: "ModifiedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suppliers_RemovedById",
+                table: "Suppliers",
+                column: "RemovedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suppliers_RestoreById",
+                table: "Suppliers",
+                column: "RestoreById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CreatedById",
+                table: "Products",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ModifiedById",
+                table: "Products",
+                column: "ModifiedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_RemovedById",
+                table: "Products",
+                column: "RemovedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_RestoreById",
+                table: "Products",
+                column: "RestoreById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Employees_CreatedById",
+                table: "Products",
+                column: "CreatedById",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Employees_ModifiedById",
+                table: "Products",
+                column: "ModifiedById",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Employees_RemovedById",
+                table: "Products",
+                column: "RemovedById",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Employees_RestoreById",
+                table: "Products",
+                column: "RestoreById",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Suppliers_Employees_CreatedById",
+                table: "Suppliers",
+                column: "CreatedById",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Suppliers_Employees_ModifiedById",
+                table: "Suppliers",
+                column: "ModifiedById",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Suppliers_Employees_RemovedById",
+                table: "Suppliers",
+                column: "RemovedById",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Suppliers_Employees_RestoreById",
+                table: "Suppliers",
+                column: "RestoreById",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Employees_CreatedById",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Employees_ModifiedById",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Employees_RemovedById",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Employees_RestoreById",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Suppliers_Employees_CreatedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Suppliers_Employees_ModifiedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Suppliers_Employees_RemovedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Suppliers_Employees_RestoreById",
+                table: "Suppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Suppliers_CreatedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Suppliers_ModifiedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Suppliers_RemovedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Suppliers_RestoreById",
+                table: "Suppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CreatedById",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ModifiedById",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_RemovedById",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_RestoreById",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Removed",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "RemovedAt",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "RemovedById",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "RestoreById",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "RestoredAt",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedById",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Removed",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "RemovedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "RemovedById",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "RestoreById",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "RestoredAt",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VehicleNumber",
+                table: "InputInvoices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "InputInvoices",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+        }
+    }
+}
