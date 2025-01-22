06bafdb3fca93c421b90999d89c94c919a3ca924
stg11077 <stg11077@gmail.com>
2025-01-22 15:29:57 +0200

    modify CreatedRequest

--- GrainElevatorAPI/DTO/Requests/CreateRequests/ProductCreateRequest.cs
+++ GrainElevatorAPI/DTO/Requests/CreateRequests/ProductCreateRequest.cs
@@ -5,7 +5,7 @@ namespace GrainElevatorAPI.DTO.Requests.CreateRequests;
 public class ProductCreateRequest
 {
     [Required(ErrorMessage = "Title is required.")]
-    [MinLength(4, ErrorMessage = "Title must be at least 4 characters long.")]
+    [MinLength(2, ErrorMessage = "Title must be at least 2 characters long.")]
     [MaxLength(20, ErrorMessage = "Title must be at least 20 characters long.")]
     public string Title { get; set; }
 }
